1b14d25a14511e41c2599b0a79d91c245043ef2b
root <junboqi@foxmail.com>
2018-01-16 10:09:24 +0800

    fix Docker file errors

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cp ../build/libs/*.jar ./
-docker image rm  qijunbo/eureka	
-docker image build -t qijunbo/eureka . 
+docker image rm  qijunbo/eureka
+docker image build -t qijunbo/eureka .
--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,5 @@
-docker run -d -v $(pwd):/tmp -p 8761:8761 qijunbo/eureka
+#!/bin/sh
+mkdir tmp
+docker stop eureka
+docker rm eureka
+docker run --name eureka -d -v $(pwd)/tmp:/tmp -p 8761:8761 qijunbo/eureka
