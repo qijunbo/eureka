4fc4c0d7c71ae450bc124e4ab2ddbddbf2db6f48
qijunbo <junboqi@foxmail.com>
2018-01-25 15:13:50 +0800

    all ways start the Docker container  on  service restart.

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 mkdir tmp
 docker stop eureka
 docker rm eureka
-docker run --name eureka -d -v $(pwd)/tmp:/tmp -p 8761:8761 qijunbo/eureka
+docker run --name eureka --restart=always -d -v $(pwd)/tmp:/tmp -p 8761:8761 qijunbo/eureka
