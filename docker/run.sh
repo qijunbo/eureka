4ce503811ac2fdc60023d8141f1921d9abeebe36
qijunbo <junboqi@foxmail.com>
2018-05-24 18:02:36 +0800

    another optional ureka.

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 mkdir tmp
+systemctl stop nginx
 docker stop eureka
 docker rm eureka
+systemctl start nginx
 docker run --name eureka --restart=always -d -v $(pwd)/tmp:/tmp -p 8761:8761 qijunbo/eureka
+#docker run --name eureka --restart=always -d -v $(pwd)/tmp:/tmp -p 8761:8761 springcloud/eureka
