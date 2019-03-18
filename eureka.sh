def124abc63247d996e59c2d9156b1ba2aff6270
tonyqi <tonyqi>
2019-03-18 18:51:44 +0800

    refine build script

--- eureka.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p /opt/eureka
-cp -f eureka.jar /opt/eureka/eureka.jar
-chmod 750 /opt/eureka/eureka.jar
-ln -sf   /opt/eureka/eureka.jar /etc/init.d/eureka
-systemctl daemon-reload
-systemctl enable eureka
-systemctl start eureka
-
